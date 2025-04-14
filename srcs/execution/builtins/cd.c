9f3b562868f11f5449dad8c64de91ea1934e746b
root <root@AidosAsusLaptop>
2025-04-14 13:56:39 +0400

    remove sanitize flag

--- srcs/execution/builtins/cd.c
+++ srcs/execution/builtins/cd.c
@@ -6,26 +6,25 @@
 /*   By: aorynbay <@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 10:54:08 by mohkhan           #+#    #+#             */
-/*   Updated: 2025/04/12 04:20:46 by aorynbay         ###   ########.fr       */
+/*   Updated: 2025/04/14 13:55:17 by aorynbay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-//incomplete ; fix the other cds too
 
 int ft_cd(char **args)
 {
-    if (!args)
-        return (1);
-    if (!args[1])
-    {
-        chdir(getenv("HOME"));
-        return (0);
-    }
-    else if (chdir(args[1]) == -1)
-    {
-        perror("cd");
-        return 1;
-    }
-    return (0);
+	if (!args)
+		return (1);
+	if (!args[1])
+	{
+		chdir(getenv("HOME"));
+		return (0);
+	}
+	else if (chdir(args[1]) == -1)
+	{
+		perror("cd");
+		return 1;
+	}
+	return (0);
 }
